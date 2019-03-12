2725b7dd83f9953b1acf417bdf9ed253c01a7eb4
juandiazsanjurjo <juandisanj@yahoo.es>
2019-03-12 18:09:08 +0100

    Creación del repositorio. Ejercicio de Spring Security básico

---
+++ src/main/java/com/example/security/demo/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.security.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// Configuración de las autorizaciones
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.formLogin()
+			.loginPage("/login")
+			.permitAll()
+			.and()
+			.logout()
+			.permitAll();
+	}
+	
+	// Creación de usuarios
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		// A través de auth se puede configurar el repositorio en el que se van a
+		// guardar los usuarios
+		auth
+			.inMemoryAuthentication()
+			.withUser("user")
+				.password("user")
+				.roles("USER")
+				.and()
+			.withUser("admin")
+				.password("admin")
+				.roles("USER", "ADMIN");
+	}
+
+	
+
+}
---
+++ src/main/resources/templates/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring Security Example</title>
+</head>
+<body>
+	<h1>Bienvenido</h1>
+	<p><a href="hello.jsp">Entrar</a></p>	
+</body>
+</html>
